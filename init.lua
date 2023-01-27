67d9869ef8ebc537dad5092a4066ea3d6c19aeb4
Asis Hallab <asis.hallab@gmail.com>
2023-01-27 13:04:42 +0100

    LocalLeader set to '\'

--- init.lua
+++ init.lua
@@ -165,7 +165,7 @@ vim.o.completeopt = 'menuone,noselect'
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = '\\'
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
